c516e65af1ec38290fa6b267facdd5b4943933cd
playbooks <git@playbooks.xyz>
2022-12-11 15:56:52 +0000

    Playbooks clone

---
+++ site/bisheng.templates.config.js
@@ -0,0 +1,13 @@
+const commonConfig = require('./bisheng.common.config');
+
+module.exports = Object.assign({
+  source: {},
+  output: './_site/templates',
+  root: '/templates/',
+  entryName: 'templates',
+  theme: './site/templates',
+  htmlTemplate: './site/templates/static/index.html',
+  port: 7113,
+  themeConfig: {
+  },
+}, commonConfig);
